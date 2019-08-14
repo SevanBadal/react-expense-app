18555442a9b4dea9683d9dee348e68e1ad97955d
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-14 16:22:44 -0400

    mock create page component

--- src/app.js
+++ src/app.js
@@ -8,10 +8,14 @@ import './styles/styles.scss';
 const ExpenseDashboardPage = () => (
   <div>This is from my dashboard component</div>
 );
+const AddExpensePage = () => (
+  <div>This is from my dashboard component</div>
+);
 const routes = (
   <BrowserRouter>
     <div>
       <Route path="/" component={ExpenseDashboardPage} />
+      <Route path="/create" component={AddExpensePage} />
     </div>
   </BrowserRouter>
 );
