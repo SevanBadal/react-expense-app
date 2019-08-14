605e534c189826824841a4f2d9914040f50ead2a
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-14 16:21:02 -0400

    mock stateless component

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
 const routes = (
   <BrowserRouter>
-
+    <Route path="/" component={ExpenseDashboardPage}/>
   </BrowserRouter>
 );
 ReactDOM.render(routes, document.getElementById('app'));
