6ad9480747cf19dd65a7448474255a0fde5c8d98
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-14 16:18:11 -0400

    router const declaration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Sevan's react boilerplate</p>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
