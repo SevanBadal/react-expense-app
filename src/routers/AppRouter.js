e0eb95d28e17aaeb9ee035e0b108bc6ed6ceedc2
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-14 19:05:24 -0400

    router broken out to routers folder

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import Header from '../components/Header';
+
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFound from '../components/NotFoundPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} exact={true} />
+        <Route path="/edit" component={EditExpensePage} exact={true} />
+        <Route path="/help" component={HelpPage} exact={true} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
