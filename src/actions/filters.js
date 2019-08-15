992b11efc8da942dbbf21af0af16c40e276bd47c
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-15 15:13:19 -0400

    changed name export syntax

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,25 +1,27 @@
 // SET_TEXT_FILTER
-export const setTextFilter = (text = '') => ({
+const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
   text
 });
 // SORT_BY_DATE
-export const sortByDate = () => ({
+const sortByDate = () => ({
   type: 'SORT_BY_DATE',
   sortBy: 'date'
 });
 // SORT_BY_AMOUNT
-export const sortByAmount = () => ({
+const sortByAmount = () => ({
   type: 'SORT_BY_AMOUNT',
   sortBy: 'amount'
 });
 // SET_START_DATE
-export const setStartDate = (date = undefined) => ({
+const setStartDate = (date = undefined) => ({
   type: 'SET_START_DATE',
   date
 });
 // SET_END_DATE
-export const setEndDate = (date = undefined) => ({
+const setEndDate = (date = undefined) => ({
   type: 'SET_END_DATE',
   date
 });
+
+export { setEndDate, setStartDate, sortByAmount, sortByDate, setTextFilter };
