268e9ff2b5d3fab13454999ce42176fdf5ad2e8e
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-15 16:46:32 -0400

    input filters dynamically and button removes data dynamically

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,15 @@
-// export a stateless functional component
-  // description, amount, createdAt
 import React from 'react';
-
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
     <h3>Description: {description}</h3>
     <p>Amount: {amount} - Created At: {createdAt}</p>
+    <button onClick={(e) => {
+      console.log('click', id);
+      dispatch(removeExpense({ id }));
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
