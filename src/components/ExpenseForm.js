4be9d6db02da8df44abb89f4c139f763299541a4
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-15 18:15:13 -0400

    add expense form with controlled inputs fields

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: '',
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+  onDateChange = (createdAt) => {
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
+  };
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }));
+    }
+  };
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  }
+  render(){
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            placeholder="amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={(day) => false }
+          />
+          <textarea
+            value={this.state.note}
+            placeholder="Add a note for your expense (optional)"
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    );
+  }
+}
