e0f29dc90e1ae1205727ef32c948590264da26db
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-15 20:31:06 -0400

    remove btn moved to EditExpensePage component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
+
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-    <h3>Description: {description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>Description: {description}</h3>
+    </Link>
     <p>Amount: {amount} - Created At: {createdAt}</p>
-    <button onClick={(e) => {
-      console.log('click', id);
-      dispatch(removeExpense({ id }));
-    }}>Remove</button>
   </div>
 );
 
