5f5e1a8d6b3cbfba0497e3fcf65758b719aaffc4
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-16 12:35:30 -0400

    dummy data moved to seperate file

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,25 +1,7 @@
 import selectExpenses from '../../selectors/expenses';
 import moment from 'moment';
+import expenses from '../fixtures/expenses';
 
-const expenses = [{
-  id: '1',
-  description: 'Gum',
-  note: '',
-  amount: 1000,
-  createdAt: 0 // unix epoch
-}, {
-  id: '2',
-  description: 'Rend',
-  note: '',
-  amount: 3445,
-  createdAt: moment(0).subtract(4, 'days').valueOf()
-}, {
-  id: '3',
-  description: 'Credit Card',
-  note: '',
-  amount: 45,
-  createdAt: moment(0).add(4, 'days').valueOf()
-}];
 test('should filter by text value', () => {
   const filters = {
     text: 'e',
