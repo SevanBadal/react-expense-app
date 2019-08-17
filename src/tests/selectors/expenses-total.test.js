e96cf79ac5525ecee4671e9bae342aed47adf559
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-17 13:14:29 -0400

    expense summary component

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,16 @@
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../fixtures/expenses';
+
+test('should return 0 if no expenses', () => {
+  const res = selectExpensesTotal([]);
+  expect(res).toBe(0);
+});
+test('should correctly add up a single value', () => {
+  const res = selectExpensesTotal([expenses[0]]);
+  expect(res).toBe(1000);
+});
+
+test('should correctly add up a multiple expenses', () => {
+  const res = selectExpensesTotal(expenses);
+  expect(res).toBe(4490);
+});
